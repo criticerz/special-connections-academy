f1fd7dd642ffa883b1cbff703738ee6e9e1d19e2
Alain <alain@Paolas-iMac.local>
2018-05-30 21:48:46 -0400

    removing links to missing pages

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ router.get('/about-us', function(req, res, next) {
   res.render('pages/about-us', { page: 'about-us', title: 'About Us' });
 });
 
-router.get('/program-information', function(req, res, next) {
-  res.render('pages/program-information', { page: 'program-information', title: 'Program Information' });
-});
+// router.get('/program-information', function(req, res, next) {
+//   res.render('pages/program-information', { page: 'program-information', title: 'Program Information' });
+// });
 
 module.exports = router;
